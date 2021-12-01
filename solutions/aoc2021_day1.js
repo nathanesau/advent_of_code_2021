6d1c3c0aa79012d6a333c7efc1e161710c8f8941
Jenny (Hongshi) Li <jennyli4239@gmail.com>
2021-11-30 21:22:43 -0800

    aoc 2021 day 1

---
+++ solutions/aoc2021_day1.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+function part1(pi) {
+    var arr = pi.split('\n').map(Number);
+    var ans = 0;
+    var pe = null;
+    for(var e of arr) {
+        if (pe != null) {
+            if (e > pe) {
+                ans += 1;
+            }
+        }
+        pe = e;
+    }
+    console.log(`part1: ${ans}`);
+}
+
+function part2(pi) {
+    var arr = pi.split('\n').map(Number);
+    var ans = 0;
+    var a = null;
+    var b = null;
+    var ps = null;
+    for(e of arr) {
+        if (a != null && b != null) {
+            var s = a + b + e;
+            if (ps != null) {
+                if (s > ps) {
+                    ans += 1;
+                }
+            }
+            ps = s;
+        }
+        b = a;
+        a = e;
+    }
+    console.log(`part2: ${ans}`);
+}
+
+var f = fs.readFileSync('aoc2021_day1.txt', 'utf8');
+var pi = f.toString();
+part1(pi);
+part2(pi);
---
+++ solutions/aoc2021_day1.py
@@ -0,0 +1,32 @@
+def part1(arr):
+    ans = 0
+    pe = None
+    for e in arr:
+        if pe is not None:
+            if e > pe:
+                ans += 1
+        pe = e
+    print("part1: ", ans)
+
+def part2(arr):
+    ans = 0
+    a = None
+    b = None
+    ps = None
+    for e in arr:
+        if a is not None and b is not None:
+            s = a + b + e
+            if ps is not None:
+                if s > ps:
+                    ans += 1
+            ps = s
+        b = a
+        a = e
+    print("part2: ", ans)
+
+with open("aoc2021_day1.txt") as f:
+    data = f.read()
+    arr = [int(line) for line in data.splitlines()]
+
+part1(arr)
+part2(arr)
